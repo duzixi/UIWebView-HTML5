66362c8d6d3f84fe082e9b2809f80ce02fa1327b
Du Zixi <duzixi@gmail.com>
2014-07-11 18:00:17 +0800

    Update UIWebView+HTML5.h
    Add methods about changing image size.

--- UIWebView+HTML5.h
+++ UIWebView+HTML5.h
@@ -3,6 +3,7 @@
 //  WebViewJS
 //
 //  Created by 杜子兮(duzixi) on 14-6-30.
+//  Edited  by 杜子兮(duzixi) on 14-7-11. 修改网页图片显示大小
 //  Copyright (c) 2014年 lanou3g.com 蓝鸥. All rights reserved.
 //
 
@@ -10,6 +11,9 @@
 
 
 @interface UIWebView (JavaScript)
+
+#pragma mark -
+#pragma mark 获取网页中的数据
 
 ///  获取某个标签的结点个数
 - (int)nodeCountOfTag:(NSString *)tag;
@@ -26,11 +30,20 @@
 ///  获取当前页面所有链接
 - (NSArray *) getOnClicks;
 
+#pragma mark -
+#pragma mark 改变网页样式和行为
+
 ///  改变背景颜色
-- (void) setBackgroundColor:(UIColor *) color;
+- (void) setBackgroundColor:(UIColor *)color;
 
 ///  为所有图片添加点击事件(网页中有些图片添加无效)
 - (void) addClickEventOnImg;
+
+///  改变所有图像的宽度
+- (void) setImgWidth:(int)size;
+
+///  改变所有图像的高度
+- (void) setImgHeight:(int)size;
 
 ///  改变指定标签的字体颜色
 - (void) setFontColor:(UIColor *) color withTag:(NSString *)tagName;
